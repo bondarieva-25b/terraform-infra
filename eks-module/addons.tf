0227a4f18bd966931ab2542fb531b07d4df3c2bc
bondarieva-25b <nadiia.bon@edu.312school.com>
2026-02-27 15:01:34 -0600

    feat: add eks add-ons version management configuration

--- eks-module/addons.tf
+++ eks-module/addons.tf
@@ -9,11 +9,77 @@ resource "aws_iam_openid_connect_provider" "eks" {
   url             = aws_eks_cluster.this.identity[0].oidc[0].issuer
 }
 
-# EBS CSI Driver IAM Role (IRSA)
 locals {
   oidc_provider     = replace(aws_eks_cluster.this.identity[0].oidc[0].issuer, "https://", "")
   oidc_provider_arn = aws_iam_openid_connect_provider.eks.arn
+
+  # All addons in one place
+  all_addons = ["coredns", "kube-proxy", "eks-pod-identity-agent", "vpc-cni", "aws-ebs-csi-driver"]
 }
+
+# ---------- Dynamic version lookup for ALL addons ----------
+
+data "aws_eks_addon_version" "this" {
+  for_each           = toset(local.all_addons)
+  addon_name         = each.value
+  kubernetes_version = aws_eks_cluster.this.version
+  most_recent        = true
+}
+
+# ---------- Simple addons (no IAM role needed) ----------
+
+resource "aws_eks_addon" "this" {
+  for_each                    = toset(["coredns", "kube-proxy", "eks-pod-identity-agent"])
+  cluster_name                = aws_eks_cluster.this.name
+  addon_name                  = each.value
+  addon_version               = data.aws_eks_addon_version.this[each.value].version
+  resolve_conflicts_on_update = "OVERWRITE"
+
+  depends_on = [aws_autoscaling_group.node]
+}
+
+# ---------- VPC CNI ----------
+
+resource "aws_iam_role" "vpc_cni" {
+  name = "${var.cluster_name}-vpc-cni-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Principal = {
+        Federated = local.oidc_provider_arn
+      }
+      Condition = {
+        StringEquals = {
+          "${local.oidc_provider}:sub" = "system:serviceaccount:kube-system:aws-node"
+          "${local.oidc_provider}:aud" = "sts.amazonaws.com"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "vpc_cni" {
+  role       = aws_iam_role.vpc_cni.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name                = aws_eks_cluster.this.name
+  addon_name                  = "vpc-cni"
+  addon_version               = data.aws_eks_addon_version.this["vpc-cni"].version
+  service_account_role_arn    = aws_iam_role.vpc_cni.arn
+  resolve_conflicts_on_update = "OVERWRITE"
+
+  depends_on = [
+    aws_autoscaling_group.node,
+    aws_iam_role_policy_attachment.vpc_cni,
+  ]
+}
+
+# ---------- EBS CSI Driver ----------
 
 resource "aws_iam_role" "ebs_csi" {
   name = "${var.cluster_name}-ebs-csi-role"
@@ -21,8 +87,8 @@ resource "aws_iam_role" "ebs_csi" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRoleWithWebIdentity"
       Effect = "Allow"
+      Action = "sts:AssumeRoleWithWebIdentity"
       Principal = {
         Federated = local.oidc_provider_arn
       }
@@ -37,38 +103,14 @@ resource "aws_iam_role" "ebs_csi" {
 }
 
 resource "aws_iam_role_policy_attachment" "ebs_csi" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
   role       = aws_iam_role.ebs_csi.name
-}
-
-# Add-ons
-resource "aws_eks_addon" "vpc_cni" {
-  cluster_name                = aws_eks_cluster.this.name
-  addon_name                  = "vpc-cni"
-  resolve_conflicts_on_update = "OVERWRITE"
-
-  depends_on = [aws_autoscaling_group.node]
-}
-
-resource "aws_eks_addon" "coredns" {
-  cluster_name                = aws_eks_cluster.this.name
-  addon_name                  = "coredns"
-  resolve_conflicts_on_update = "OVERWRITE"
-
-  depends_on = [aws_autoscaling_group.node]
-}
-
-resource "aws_eks_addon" "kube_proxy" {
-  cluster_name                = aws_eks_cluster.this.name
-  addon_name                  = "kube-proxy"
-  resolve_conflicts_on_update = "OVERWRITE"
-
-  depends_on = [aws_autoscaling_group.node]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
 
 resource "aws_eks_addon" "ebs_csi" {
   cluster_name                = aws_eks_cluster.this.name
   addon_name                  = "aws-ebs-csi-driver"
+  addon_version               = data.aws_eks_addon_version.this["aws-ebs-csi-driver"].version
   service_account_role_arn    = aws_iam_role.ebs_csi.arn
   resolve_conflicts_on_update = "OVERWRITE"
 
@@ -76,12 +118,4 @@ resource "aws_eks_addon" "ebs_csi" {
     aws_autoscaling_group.node,
     aws_iam_role_policy_attachment.ebs_csi,
   ]
-}
-
-resource "aws_eks_addon" "pod_identity" {
-  cluster_name                = aws_eks_cluster.this.name
-  addon_name                  = "eks-pod-identity-agent"
-  resolve_conflicts_on_update = "OVERWRITE"
-
-  depends_on = [aws_autoscaling_group.node]
 }
