2e290013e98d6f7011d4b70f31ef392b11894b91
bondarieva-25b <nadiia.bon@edu.312school.com>
2026-03-01 20:53:45 -0600

    feat: add karpenter tf code

--- root-module/outputs.tf
+++ root-module/outputs.tf
@@ -63,3 +63,20 @@ output "proshop_secrets_role_arn" {
 output "proshop_secret_name" {
   value = aws_secretsmanager_secret.proshop_backend.name
 }
+
+# Karpenter Outputs
+output "karpenter_controller_role_arn" {
+  value = aws_iam_role.karpenter_controller.arn
+}
+
+output "karpenter_node_role_name" {
+  value = aws_iam_role.karpenter_node.name
+}
+
+output "karpenter_instance_profile_name" {
+  value = aws_iam_instance_profile.karpenter_node.name
+}
+
+output "karpenter_queue_name" {
+  value = aws_sqs_queue.karpenter.name
+}
